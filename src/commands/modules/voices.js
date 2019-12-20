d8d6eaa56f4f797eb3e1d725f250a2c2cfc9becd
aaron <aaron@aaron-m.co.nz>
2019-12-20 19:24:12 +1300

    Leela: That's the ONLY thing about being a slave.

--- src/commands/modules/voices.js
+++ src/commands/modules/voices.js
@@ -24,11 +24,7 @@ function listVoices(msg) {
   const exampleEmbed = {
     color: 0x0099ff,
     title: `Click to find voices${msg.content ? " for: " +msg.content + "." : "."}`,
-<<<<<<< HEAD
-    url: `https://voices.talkbot.dev/?chr=${encodeURI(msg.server.command_char || auth.command_char)}&find=${encodeURI(msg.content)}`,
-=======
     url: `https://voices.talkbot.dev/?chr=${encodeURI(commands.getCommandChar(msg.server))}&find=${encodeURI(msg.content)}`,
->>>>>>> 1e9ff8e879520be6f8ac9b71742d3950c446b1d4
     description: 'talkbot voice and voice sample database.',
     thumbnail: {
       url: 'https://voices.talkbot.dev/img/face_200.png',
