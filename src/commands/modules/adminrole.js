ba323d8dd69a18f6b10472857dca0d9ca9c5785c
Wooto Smash <loafburg@gmail.com>
2020-02-29 18:36:30 +0800

    Morbo: Morbo can't understand his teleprompter because he forgot how you say that letter that's shaped like a man wearing a hat.

---
+++ src/commands/modules/adminrole.js
@@ -0,0 +1,41 @@
+/*jshint esversion: 9 */
+const Command = require('@models/Command'),
+  config = require("@auth"),
+  util = require("util"),
+  Common = require('@helpers/common');
+
+class AdminRole extends Command {
+
+    get group () {  return 'control'; }
+    get hidden () { return true; }
+
+    execute ({input}) {
+        if (!input.args.length) return input.il8nResponse('adminrole.usage');
+        if (!input.ownerIsDev() && !input.ownerCanManageTheServer()) return input.il8nResponse('adminrole.notbotowner');
+
+        let roles = input.message.mentions.roles.concat(input.getNonSnowflakeRoles());
+        if ( roles.size == 0) {
+          input.il8nResponse('adminrole.none');
+          return;
+        }
+        
+        input.server.adminrole = roles.first().id;
+        
+        input.il8nResponse('adminrole.okay');
+    }
+}
+
+//registration
+exports.register = (commands) => {
+    commands.addAll([
+        AdminRole.command
+    ]);
+};
+  
+exports.unRegister = (commands) => {
+    commands.removeAll([
+        AdminRole.command,
+    ]);
+};
+  
+exports.AdminRole = AdminRole;
