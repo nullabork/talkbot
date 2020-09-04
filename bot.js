502fa9deb184f53055274ff07f117e37537a930d
Wooto Smash <loafburg@gmail.com>
2020-09-04 09:00:11 +0800

    fixed defaults

--- bot.js
+++ bot.js
@@ -12,95 +12,106 @@
  */
 (async () => {
   //npm imports
-  require('module-alias/register');
+  require("module-alias/register");
 
   //helpers
-  var commands = require('@commands'),
-    figlet = require('figlet'),
-    botStuff = require('@helpers/bot-stuff'),
-    Common = require('@helpers/common'),
-    testing = require('@helpers/runtime-testing');
+  var commands = require("@commands"),
+    figlet = require("figlet"),
+    botStuff = require("@helpers/bot-stuff"),
+    Common = require("@helpers/common"),
+    testing = require("@helpers/runtime-testing");
 
   //models
-  var world = require('@models/World');
+  var world = require("@models/World");
 
   var bot = botStuff.bot;
 
   // runtime testing
-  testing.TestIfChildProcessIsWorkingHowDiscordJSNeedsItTo();
   await testing.TestIfTTSAPIServicesAreConfigured();
   testing.TestIfNodeOpusIsInstalled();
 
   // FANCY SPLASH SCREEN
-  figlet('TalkBot', (err, data) => console.log(data));
+  figlet("TalkBot", (err, data) => console.log(data));
 
   // when the server is ready to go
-  bot.on('ready', () => {
-    Common.out('Logged in as: ' + bot.user.username + ' - (' + bot.user.id + ')');
-    Common.error('LOG CHECKPOINT: THE BOT STARTED. THIS IS NOT AN ERROR MESSAGE');
+  bot.on("ready", () => {
+    Common.out(
+      "Logged in as: " + bot.user.username + " - (" + bot.user.id + ")"
+    );
+    Common.error(
+      "LOG CHECKPOINT: THE BOT STARTED. THIS IS NOT AN ERROR MESSAGE"
+    );
     world.startup();
   });
 
   // when the bot is added to new servers
-  bot.on('guildCreate', guild => {
+  bot.on("guildCreate", (guild) => {
     try {
       //add the relationships
       world.addServer(guild);
+    } catch (ex) {
+      Common.error(ex);
     }
-    catch(ex) { Common.error(ex); }
   });
 
   // when the bot is removed from servers
-  bot.on('guildDelete', guild => {
+  bot.on("guildDelete", (guild) => {
     try {
       world.removeServer(guild);
+    } catch (ex) {
+      Common.error(ex);
     }
-    catch(ex) { Common.error(ex); }
   });
 
   // when a member is removed unfollow
-  bot.on('guildMemberRemove', member => {
+  bot.on("guildMemberRemove", (member) => {
     try {
       var server = world.servers[member.guild.id];
-      if (server.isMaster(member))
-      {
+      if (server.isMaster(member)) {
         server.release();
       }
+    } catch (ex) {
+      Common.error(ex);
     }
-    catch(ex) { Common.error(ex); }
   });
 
   // handle voice state updates
-  bot.on('voiceStateUpdate', (oldMember, newMember) => {
+  bot.on("voiceStateUpdate", (oldMember, newMember) => {
     if (!oldMember) return;
 
     try {
       var server = world.servers[oldMember.guild.id];
-      if (!server.isMaster(oldMember))
-        return;
+      if (!server.isMaster(oldMember)) return;
 
       // they've changed voice channels
-      if ( oldMember.voiceChannel && (!newMember.voiceChannel || !newMember.voiceChannel.joinable)) { // || oldMember.voiceChannel.id != newMember.voiceChannel.id
+      if (
+        oldMember.voiceChannel &&
+        (!newMember.voiceChannel || !newMember.voiceChannel.joinable)
+      ) {
+        // || oldMember.voiceChannel.id != newMember.voiceChannel.id
         server.release();
-      }
-      else if ( oldMember.voiceChannel && newMember.voiceChannel && oldMember.voiceChannel.id != newMember.voiceChannel.id )
-      {
+      } else if (
+        oldMember.voiceChannel &&
+        newMember.voiceChannel &&
+        oldMember.voiceChannel.id != newMember.voiceChannel.id
+      ) {
         server.switchVoiceChannel(newMember.voiceChannel);
       }
+    } catch (ex) {
+      Common.error(ex);
     }
-    catch(ex) { Common.error(ex); }
   });
 
   // when messages come in
-  bot.on('message', message => {
+  bot.on("message", (message) => {
     try {
       // ignore message from myself
-      if ( message.member && message.member.id == bot.user.id ) return;
+      if (message.member && message.member.id == bot.user.id) return;
 
       var server = null;
-      
+
       // if its in a server and not a DM
-      if ( message.guild ) {
+      if (message.guild) {
         server = world.servers[message.guild.id];
 
         if (server == null) {
@@ -112,45 +123,52 @@
       // is the message a command?
       if (commands.isCommand(message, server)) {
         commands.process(message, server, world);
-      } else if ( message.member ) {
+      } else if (message.member) {
         // say it out loud
         server.speak(message);
       }
-    }
-    catch(ex) {
+    } catch (ex) {
       Common.error(ex);
     }
   });
 
   // if we get disconnected???
-  bot.on('disconnect', evt => {
+  bot.on("disconnect", (evt) => {
     try {
       world.saveAll();
       world.dispose();
-      Common.out('Disconnected, reconnecting');
+      Common.out("Disconnected, reconnecting");
       Common.out(evt);
       botStuff.connect();
+    } catch (ex) {
+      Common.error(ex);
     }
-    catch(ex) { Common.error(ex); }
   });
 
   // capture a whole pile of useful information
-  bot.on('error',            Common.error);
-  bot.on('guildUnavailable', guild    => Common.error('guild unavailable: ' + guild.id));
-  bot.on('rateLimit',        info     => { Common.error('rate limited'); Common.error(info); });
-  bot.on('reconnecting',     ()       => Common.error('reconnecting'));
-  bot.on('resume',           replayed => Common.error('resume: ' + replayed));
-  bot.on('warn',             info     => Common.error('warn:' + info));
+  bot.on("error", Common.error);
+  bot.on("guildUnavailable", (guild) =>
+    Common.error("guild unavailable: " + guild.id)
+  );
+  bot.on("rateLimit", (info) => {
+    Common.error("rate limited");
+    Common.error(info);
+  });
+  bot.on("reconnecting", () => Common.error("reconnecting"));
+  bot.on("resume", (replayed) => Common.error("resume: " + replayed));
+  bot.on("warn", (info) => Common.error("warn:" + info));
 
-  bot.on('disconnect',             info     => Common.error('disconnect:' + info));
-  
+  bot.on("disconnect", (info) => Common.error("disconnect:" + info));
+
   // ctrl-c
-  process.on('SIGINT', () => world.kill('SIGINT'));
+  process.on("SIGINT", () => world.kill("SIGINT"));
 
   // something goes wrong we didnt think of or having got around to putting a band-aid fix on
-  process.on('uncaughtException', err => { Common.error(err); world.kill('uncaughtException: ' + err.message); });
+  process.on("uncaughtException", (err) => {
+    Common.error(err);
+    world.kill("uncaughtException: " + err.message);
+  });
 
   // start it up!
   botStuff.connect();
-
 })(); // wtf async.
--- src/services/tts/AzureTextToSpeechAPI.js
+++ src/services/tts/AzureTextToSpeechAPI.js
@@ -1,15 +1,14 @@
 /*jshint esversion: 9 */
-const Common = require('@helpers/common'),
-  TextToSpeechService = require('@services/TextToSpeechService'),
+const Common = require("@helpers/common"),
+  TextToSpeechService = require("@services/TextToSpeechService"),
   auth = require("@auth"),
-  rp = require('request-promise'),
-  prism = require('prism-media'),
-  lame = require('lame'),
-  samplerate = require('node-libsamplerate'),
-  xmlbuilder = require('xmlbuilder');  
+  rp = require("request-promise"),
+  prism = require("prism-media"),
+  lame = require("lame"),
+  samplerate = require("node-libsamplerate"),
+  xmlbuilder = require("xmlbuilder");
 
 class AzureTextToSpeechAPI extends TextToSpeechService {
-
   // name of the service - eg. google, amazon, azure, watson
   get shortname() {
     return "azure";
@@ -30,11 +29,13 @@ class AzureTextToSpeechAPI extends TextToSpeechService {
     return auth.tts.azure.limit;
   }
 
-  get format() { return "pcm"; }
+  get format() {
+    return "pcm";
+  }
 
-  get accessToken() { 
+  get accessToken() {
     return this.token;
-  } 
+  }
   set accessToken(value) {
     this.token = value;
   }
@@ -45,27 +46,34 @@ class AzureTextToSpeechAPI extends TextToSpeechService {
    * Should exit the process if this is not configured correctly
    */
   async startupTests() {
-
     const self = this;
     const subscriptionKey = auth.tts.azure.subscriptionKey;
-    
+
     if (!subscriptionKey) {
-      console.log('Config variable azure.subscriptionKey for your subscription key is not set.');
+      console.log(
+        "Config variable azure.subscriptionKey for your subscription key is not set."
+      );
       process.exit(1);
     }
 
     try {
       self.accessToken = await self.getAccessToken(subscriptionKey);
-      
+
       // accessToken expires after an hour so we need to renew it
-      setInterval(() => self.getAccessToken(auth.tts.azure.subscriptionKey).then((token) => { self.accessToken = token }), 10 * 60 * 1000);
+      setInterval(
+        () =>
+          self.getAccessToken(auth.tts.azure.subscriptionKey).then((token) => {
+            self.accessToken = token;
+          }),
+        10 * 60 * 1000
+      );
       var v = await self.getVoicesFromAzure(self.accessToken);
       AzureTextToSpeechAPI.voices = self.processVoices(v);
     } catch (err) {
       console.log(`Something went wrong: ${err}`);
       process.exit(1);
-    }    
-    console.log("Loaded the Azure TTS API credentials OK.");    
+    }
+    console.log("Loaded the Azure TTS API credentials OK.");
   }
 
   /**
@@ -76,17 +84,20 @@ class AzureTextToSpeechAPI extends TextToSpeechService {
    *
    * @return  {[type]}  [return request object for this API]
    */
-  buildRequest (msg, settings, server) {
-
+  buildRequest(msg, settings, server) {
     // Create the SSML request.
-    let xml_body = xmlbuilder.create('speak')
-        .att('version', '1.0')
-        .att('xmlns','https://www.w3.org/2001/10/synthesis')
-        .att('xml:lang', settings.language ? settings.language.toLowerCase() : 'en-US')
-        .ele('voice')
-        .att('name', settings.name )
-        .txt(msg)
-        .end();
+    let xml_body = xmlbuilder
+      .create("speak")
+      .att("version", "1.0")
+      .att("xmlns", "https://www.w3.org/2001/10/synthesis")
+      .att(
+        "xml:lang",
+        settings.language ? settings.language.toLowerCase() : "en-US"
+      )
+      .ele("voice")
+      .att("name", settings.name)
+      .txt(msg)
+      .end();
 
     // Convert the XML into a string to send in the TTS request.
     let request = xml_body.toString();
@@ -99,35 +110,33 @@ class AzureTextToSpeechAPI extends TextToSpeechService {
    * @param {*} request
    * @param {*} callback (err, audio) => {...}
    */
-  getAudioContent (request, callback) {
-
+  getAudioContent(request, callback) {
     var self = this;
     let options = {
-        method: 'POST',
-        baseUrl: self.baseUrl,
-        url: 'cognitiveservices/v1',
-        headers: {
-            'Authorization': 'Bearer ' + self.accessToken,
-            'cache-control': 'no-cache',
-            'User-Agent': 'AzureTextToSpeechAPI',
-            'X-Microsoft-OutputFormat': 'audio-24khz-160kbitrate-mono-mp3',
-            'Content-Type': 'application/ssml+xml'
-        },
-        body: request
-    }
+      method: "POST",
+      baseUrl: self.baseUrl,
+      url: "cognitiveservices/v1",
+      headers: {
+        Authorization: "Bearer " + self.accessToken,
+        "cache-control": "no-cache",
+        "User-Agent": "AzureTextToSpeechAPI",
+        "X-Microsoft-OutputFormat": "audio-24khz-160kbitrate-mono-mp3",
+        "Content-Type": "application/ssml+xml",
+      },
+      body: request,
+    };
 
     let p = rp(options)
-      .on('response', response => {
+      .on("response", (response) => {
         if (response.statusCode === 200) {
-
           var ld = new lame.Decoder({
-            sampleRate: 24000,       
-            channels: lame.MONO,  
-            signed: true,         
-            float: false,         
-            bitDepth: 16,    
+            sampleRate: 24000,
+            channels: lame.MONO,
+            signed: true,
+            float: false,
+            bitDepth: 16,
           });
-          
+
           var resample = new samplerate({
             // Value can be from 0 to 4 or using enum. 0 is the best quality and the slowest.
             type: samplerate.SRC_SINC_MEDIUM_QUALITY,
@@ -140,17 +149,28 @@ class AzureTextToSpeechAPI extends TextToSpeechService {
             // Desired sample rate
             toRate: 48000,
             // Desired bit depth. Valid values: 16 or 32
-            toDepth: 16
+            toDepth: 16,
           });
 
-          callback(null, p.pipe(ld).pipe(resample).pipe(new prism.opus.Encoder({rate: 48000, channels: 1, frameSize: 960 })));
-        }
-        else {
+          callback(
+            null,
+            p
+              .pipe(ld)
+              .pipe(resample)
+              .pipe(
+                new prism.opus.Encoder({
+                  rate: 48000,
+                  channels: 1,
+                  frameSize: 960,
+                })
+              )
+          );
+        } else {
           Common.error(response);
-          callback(new Error('HTTP ERROR: ' + response.statusCode));
+          callback(new Error("HTTP ERROR: " + response.statusCode));
         }
       })
-      .on('error', err => callback(err));
+      .on("error", (err) => callback(err));
     return p;
   }
 
@@ -159,51 +179,63 @@ class AzureTextToSpeechAPI extends TextToSpeechService {
   }
 
   getDefaultVoice(gender, lang_code) {
-    return AzureTextToSpeechAPI.voices[0].voice;
+    if (!AzureTextToSpeechAPI.voices || !AzureTextToSpeechAPI.voices.length)
+      return "en-GB-Susan";
+    var voices = AzureTextToSpeechAPI.voices.filter(
+      (v) => v.code == lang_code && v.gender == gender
+    );
+    if (voices.length) return voices[0].voice;
+    return "en-GB-Susan";
   }
 
   getRandomVoice(randnum, gender, lang_code) {
-    if ( !randnum ) randnum = Math.random() * 1000000; 
-    var voices = AzureTextToSpeechAPI.voices.filter(voice => (!lang_code || voice.code == lang_code) && (!gender || voice.gender == gender));
+    if (!randnum) randnum = Math.random() * 1000000;
+    var voices = AzureTextToSpeechAPI.voices.filter(
+      (voice) =>
+        (!lang_code || voice.code == lang_code) &&
+        (!gender || voice.gender == gender)
+    );
 
     return voices[randnum % voices.length].voice;
   }
 
   getVoicesFromAzure(accessToken) {
     let options = {
-      method: 'GET',
-      url: 'https://eastus.tts.speech.microsoft.com/cognitiveservices/voices/list',
+      method: "GET",
+      url:
+        "https://eastus.tts.speech.microsoft.com/cognitiveservices/voices/list",
       headers: {
-        'Authorization': 'Bearer ' + accessToken,
-        'Content-Type': 'application/json'
+        Authorization: "Bearer " + accessToken,
+        "Content-Type": "application/json",
       },
-      transform: body => JSON.parse(body),
-      transform2xxOnly: true
+      transform: (body) => JSON.parse(body),
+      transform2xxOnly: true,
     };
 
     let p = rp(options)
-      .then(response => { return response; })
-      .catch(err => Common.error(err));
+      .then((response) => {
+        return response;
+      })
+      .catch((err) => Common.error(err));
     return p;
-  }  
+  }
 
   processVoices(voices) {
+    let v = [];
 
-    var v = [];
-
-    for ( var index in voices ) {
+    for (var index in voices) {
       var voice = voices[index];
 
       var map = this.voiceMap ? this.voiceMap[voice.ShortName] : null;
 
       var n = {
-        provider:    this.shortname,
-        voice:       voice.ShortName,
-        code:        voice.Locale,
-        translate:   map ? map.translate : voice.Locale.substring(0,2),
-        gender:      map ? map.gender : this.standardiseGender(voice.Gender),
+        provider: this.shortname,
+        voice: voice.ShortName,
+        code: voice.Locale,
+        translate: map ? map.translate : voice.Locale.substring(0, 2),
+        gender: map ? map.gender : this.standardiseGender(voice.Gender),
         voice_alias: map ? map.alias : voice.ShortName,
-        language:    map ? map.language : voice.Locale
+        language: map ? map.language : voice.Locale,
       };
 
       v.push(n);
@@ -213,23 +245,23 @@ class AzureTextToSpeechAPI extends TextToSpeechService {
   }
 
   standardiseGender(token) {
-    if ( !token) return '';
-    if ( token.substring(0,1) == 'F') return 'FEMALE';
-    if ( token.substring(0,1) == 'M') return 'MALE';
-    return '';
+    if (!token) return "";
+    if (token.substring(0, 1) == "F") return "FEMALE";
+    if (token.substring(0, 1) == "M") return "MALE";
+    return "";
   }
 
   // gets a temporary token that lasts a short while to access the service
   async getAccessToken(subscriptionKey) {
     let options = {
-      method: 'POST',
-      uri: 'https://eastus.api.cognitive.microsoft.com/sts/v1.0/issuetoken',
+      method: "POST",
+      uri: "https://eastus.api.cognitive.microsoft.com/sts/v1.0/issuetoken",
       headers: {
-        'Ocp-Apim-Subscription-Key': subscriptionKey
-      }
+        "Ocp-Apim-Subscription-Key": subscriptionKey,
+      },
     };
     return rp(options);
-  }  
+  }
 }
 
 module.exports = AzureTextToSpeechAPI;
