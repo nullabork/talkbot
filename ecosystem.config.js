5ff72b6d3ede09c17c5530bfc391f70035e330fc
fax <fax@fax.com>
2020-11-30 00:20:26 +1300

    fixed a thing

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -24,5 +24,17 @@ module.exports = {
                 GOOGLE_APPLICATION_CREDENTIALS: '/root/.google/auth.json',
             },
         },
+        {
+            name: 'serverprod',
+            script: './bot.js',
+            exp_backoff_restart_delay: 100,
+            error_file: 'err.log',
+            out_file: 'out.log',
+            log_file: 'combined.log',
+            env: {
+                NODE_ENV: 'production',
+                GOOGLE_APPLICATION_CREDENTIALS: '/root/.google/auth.json',
+            },
+        },
     ],
 };
